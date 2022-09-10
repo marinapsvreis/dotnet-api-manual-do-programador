5e3e0c3ce8444c9a62b1fd31e286c657b6d17da3
Marina Portugal <marinapsvreis@gmail.com>
2022-09-10 17:19:39 -0300

    Estrutura do Projeto e Entidade Pessoa

---
+++ MP.ApiDotNet6.Domain/Entities/Person.cs
@@ -0,0 +1,41 @@
+﻿using MP.ApiDotNet6.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Domain.Entities
+{
+    public sealed class Person //SEALED não permite que a classe seja herdada
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        public Person(string name, string document, string phone)
+        {
+            Validation(name, document, phone);
+        }
+
+        public Person(int id, string document, string name, string phone)
+        {
+            DomainValidationException.When(id < 0, "Id deve ser maior que zero.");
+            Id = id;
+            Validation(name, document, phone);
+        }
+
+        private void Validation(string document, string name, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Celular deve ser informado");
+
+            Name = name;
+            Document = document;
+            Phone = phone;
+
+        }
+    }
+}
