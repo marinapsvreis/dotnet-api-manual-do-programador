d0aed0ca21f3d5522cd93e1be18f9c36a4d1a9de
Marina Portugal <marinapsvreis@gmail.com>
2022-09-10 19:01:57 -0300

    DTO de Pessoa e Validator

---
+++ MP.ApiDotNet6.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Application.DTOs
+{
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ MP.ApiDotNet6.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Application.DTOs.Validations
+{
+    public class PersonDTOValidator : AbstractValidator<PersonDTO>
+    {
+        public PersonDTOValidator()
+        {
+            RuleFor(x => x.Document)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Documento deve ser informado.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Nome deve ser informado");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Celular deve ser informado");
+        }
+    }
+}
