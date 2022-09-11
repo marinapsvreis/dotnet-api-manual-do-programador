ef9ad5dc8747a4e520890264863edb3ad9aa653a
Marina Portugal <marinapsvreis@gmail.com>
2022-09-11 19:11:57 -0300

    Criando os metodos de update e delete de Pessoa

--- MP.ApiDotNet6.Application/Services/Interfaces/IPersonService.cs
+++ MP.ApiDotNet6.Application/Services/Interfaces/IPersonService.cs
@@ -12,5 +12,7 @@ namespace MP.ApiDotNet6.Application.Services.Interfaces
         Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO);
         Task<ResultService<ICollection<PersonDTO>>> GetAsync();
         Task<ResultService<PersonDTO>> GetByIdAsync(int id);
+        Task<ResultService> UpdateAsync(PersonDTO personDTO);
+        Task<ResultService> DeleteAsync(int id);
     }
 }
--- MP.ApiDotNet6.Application/Services/PersonService.cs
+++ MP.ApiDotNet6.Application/Services/PersonService.cs
@@ -39,6 +39,16 @@ namespace MP.ApiDotNet6.Application.Services
             return ResultService.Ok<PersonDTO>(_mapper.Map<PersonDTO>(data));
         }
 
+        public async Task<ResultService> DeleteAsync(int id)
+        {
+            var person = await _personRepository.GetByIdAsync(id);
+            if (person == null)
+                return ResultService.Fail("Pessoa não encontrada");
+
+            await _personRepository.DeleteAsync(person);
+            return ResultService.Ok($"Pessoa do id:{id} foi deletada");
+        }
+
         public async Task<ResultService<ICollection<PersonDTO>>> GetAsync()
         {
             var people = await _personRepository.GetPeopleAsync();
@@ -51,6 +61,28 @@ namespace MP.ApiDotNet6.Application.Services
             if (person == null)
                 return ResultService.Fail<PersonDTO>("Pessoa não encontrada");
             return ResultService.Ok(_mapper.Map<PersonDTO>(person));
+        }
+
+        public async Task<ResultService> UpdateAsync(PersonDTO personDTO)
+        {
+            if (personDTO == null)
+                return ResultService.Fail("Objeto deve ser informado");
+
+            var validation = new PersonDTOValidator().Validate(personDTO);
+
+            if (!validation.IsValid)
+                return ResultService.RequestError("Problema com a validação dos campos", validation);
+
+            var person = await _personRepository.GetByIdAsync(personDTO.Id);
+
+            if (person == null)
+                return ResultService.Fail("Pessoa não encontrada");
+
+            //var person = _mapper.Map<Person>(personDTO); - assim fica quando vai inserir
+            person = _mapper.Map<PersonDTO, Person>(personDTO, person); //assim seria na edição - mantendo o estado da pessoa
+            await _personRepository.EditAsync(person);
+
+            return ResultService.Ok("Pessoa editada");
         }
     }
 }
